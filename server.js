df67ac2bbd4ce33a67f9dd6ed204eb724450c5aa
Milansuman <milansuman2016@gmail.com>
2024-02-29 11:34:25 +0530

    add paths and connect to backend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const user = require("./controllers/user");
 
@@ -9,6 +10,18 @@ app.use(cookieParser());
 app.use(express.static("public/"));
 
 app.use("/api/user", user);
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views", "login.html"));
+})
+
+app.get("/pixel", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views", "pixel.html"));
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views", "index.html"));
+})
 
 app.listen(9000, () => {
     console.log("Listening on http://localhost:9000");
