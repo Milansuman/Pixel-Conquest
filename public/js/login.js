c1b235d9f22b64ffb2d52e2390c761b2b8ec922f
Milansuman <milansuman2016@gmail.com>
2024-03-01 06:36:58 +0530

    fix login

--- public/js/login.js
+++ public/js/login.js
@@ -1,6 +1,13 @@
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (ev) => {
+form.addEventListener("submit", async (ev) => {
     ev.preventDefault();
-    
+    const data = new FormData(form)
+    try{
+        await post("/api/user/login", data)
+        window.location.href = "/pixel"
+    }catch(err){
+        window.location.href = "/login"
+    }
+
 })
--- public/js/network.js
+++ public/js/network.js
@@ -1,7 +1,9 @@
 async function get(url){
     return new Promise((resolve, reject) => {
-        fetch(url).then(response => response.json())
-                    .then(data => resolve(data))
+        fetch(url).then(response => {
+            if(response.status !== 200) throw response.statusText
+            response.json()
+        }).then(data => resolve(data))
                     .catch(err => reject(err));
     });
 }
@@ -10,8 +12,14 @@ async function post(url, data=(new FormData())){
     return new Promise((resolve, reject) => {
         fetch(url, {
             method: "POST",
-            body: data
-        }).then(response => response.json())
+            body: (new URLSearchParams(data)).toString(),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).then(response => {
+            if(response.status !== 200) throw response.statusText
+            response.json()
+        })
         .then(data => resolve(data))
         .catch(err => reject(err));
     })
